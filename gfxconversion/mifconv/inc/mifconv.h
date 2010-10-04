0fa10fa722b8073e1c5c0fddf92bbe1304a0439b
hgs <none@none>
2010-10-04 10:17:33 +0300

    201039

--- gfxconversion/mifconv/inc/mifconv.h
+++ gfxconversion/mifconv/inc/mifconv.h
@@ -38,8 +38,8 @@ using namespace std;
 using namespace MifConvDefs;
 
 // Mifconv version
-static const MifConvString MifConvVersion("3.2.1");
-static const MifConvString MifConvDate("12th March 2010");
+static const MifConvString MifConvVersion("3.3.0");
+static const MifConvString MifConvDate("9th August 2010");
 static const MifConvString MifConvYears("2010");
 
 // Mifconv return values:
